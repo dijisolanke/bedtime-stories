50df3d43a912116f3ade5585490f33f271f8799c
diji <diji.solanke@yahoo.com>
2025-07-04 02:52:34 +0100

    Fix: Replace catch-all admin route with page.tsx

--- app/admin/[[...index]]/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { NextStudio } from "next-sanity/studio";
-import config from "../../../sanity/stories-for-mo/sanity.config";
-
-export default function AdminPage() {
-  return <NextStudio config={config} />;
-}
